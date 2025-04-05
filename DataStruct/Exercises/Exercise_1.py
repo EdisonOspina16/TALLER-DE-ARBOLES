fe8eb07ca8596ed0fce1e699be6f1d9533482b51
Edison <edison100ospina@gmail.com>
2025-04-05 13:46:49 -0500

    README.md

--- DataStruct/Exercises/Exercise_1.py
+++ DataStruct/Exercises/Exercise_1.py
@@ -1,5 +1,41 @@
-from DataStruct.Classes import *
+from DataStruct.Classes.BinaryTree import BinarySearchTree, Node
+import json
 
-def exercise_1():
-    pass
+
+def diagnostico(nodo):
+    if "diagnostico" in nodo:
+        print(f"Diagnóstico: {nodo['diagnostico']}")
+        return nodo['diagnostico']
+
+    respuesta = input(f"{nodo['pregunta']} (si/no): ").strip().lower()
+
+    if respuesta == "si":
+        return diagnostico(nodo["si"])
+    elif respuesta == "no":
+        return diagnostico(nodo["no"])
+    else:
+        print("Por favor, responde con 'si' o 'no'.")
+        return diagnostico(nodo)
+
+
+def recorrido_postorden(nodo):
+    if "diagnostico" in nodo:
+        return [nodo["diagnostico"]]
+
+    diagnosticos = []
+    if "si" in nodo:
+        diagnosticos.extend(recorrido_postorden(nodo["si"]))
+    if "no" in nodo:
+        diagnosticos.extend(recorrido_postorden(nodo["no"]))
+
+    return diagnosticos
+
+
+
+with open("arbol_diagnostico_medico.json", "r", encoding="utf-8") as file:
+    arbol = json.load(file)
+
+
+print("Bienvenido al sistema de diagnóstico médico.")
+diagnostico(arbol)
 
